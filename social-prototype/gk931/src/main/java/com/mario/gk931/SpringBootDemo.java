a01dec21c74c42dcb834f3ccb22c9a300cd8bdba
Mario Fentler <mfentler@student.tgm.ac.at>
2018-11-22 16:31:56 +0100

    Added try to fix errors - noch nicht fertig bitte andere uebung zuerst korrigieren

---
+++ social-prototype/gk931/src/main/java/com/mario/gk931/SpringBootDemo.java
@@ -0,0 +1,35 @@
+package com.mario.gk931;
+
+import java.security.Principal;
+import org.springframework.boot.*;
+import org.springframework.boot.autoconfigure.*;
+import org.springframework.boot.autoconfigure.security.oauth2.client.EnableOAuth2Sso;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.web.bind.annotation.*;
+@SpringBootApplication
+@RestController
+@EnableOAuth2Sso
+public class SpringBootDemo extends WebSecurityConfigurerAdapter {
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .csrf().disable()
+                .antMatcher("/**")
+                .authorizeRequests()
+                .antMatchers("/", "/login**", "/webjars/**")
+                .permitAll()
+                .anyRequest()
+                .authenticated()
+                .and().logout().logoutSuccessUrl("/").permitAll();
+    }
+
+    @RequestMapping("/user")
+    public Principal user(Principal principal) {
+        return principal;
+    }
+
+    public static void main(String[] args) throws Exception {
+        SpringApplication.run(SpringBootDemo.class, args);
+    }
+}
