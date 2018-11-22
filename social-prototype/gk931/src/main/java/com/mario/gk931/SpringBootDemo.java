6c7e91c95b0dfa2b1141081cdf49a19ff24480dd
Mario Fentler <mfentler@student.tgm.ac.at>
2018-11-22 16:40:43 +0100

    Clean OAuth2 Auth with Facebook

--- social-prototype/gk931/src/main/java/com/mario/gk931/SpringBootDemo.java
+++ social-prototype/gk931/src/main/java/com/mario/gk931/SpringBootDemo.java
@@ -7,29 +7,31 @@ import org.springframework.boot.autoconfigure.security.oauth2.client.EnableOAuth
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.web.bind.annotation.*;
+
 @SpringBootApplication
-@RestController
 @EnableOAuth2Sso
-public class SpringBootDemo extends WebSecurityConfigurerAdapter {
-    @Override
-    protected void configure(HttpSecurity http) throws Exception {
-        http
-                .csrf().disable()
-                .antMatcher("/**")
-                .authorizeRequests()
-                .antMatchers("/", "/login**", "/webjars/**")
-                .permitAll()
-                .anyRequest()
-                .authenticated()
-                .and().logout().logoutSuccessUrl("/").permitAll();
-    }
+@RestController
+public class SpringBootDemo extends WebSecurityConfigurerAdapter{
 
     @RequestMapping("/user")
     public Principal user(Principal principal) {
         return principal;
     }
 
-    public static void main(String[] args) throws Exception {
+    public static void main(String[] args) {
         SpringApplication.run(SpringBootDemo.class, args);
     }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .antMatcher("/**")
+                .authorizeRequests()
+                .antMatchers("/", "/login**", "/webjars/**", "/error**")
+                .permitAll()
+                .anyRequest()
+                .authenticated();
+    }
+
+
 }
